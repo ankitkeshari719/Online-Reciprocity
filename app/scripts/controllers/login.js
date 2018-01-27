ce32691e471d32dc90f1c3d3c9b2af0129d7852d
Ankit Keshari <GS-1544@GSLAB.COM>
2018-01-27 19:37:00 +0530

    login credencial

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,5 +1,40 @@
 angular
     .module('onlineReciprocityApp')
     .controller('LoginCtrl',function ($scope) {
+        $scope.loginError = false;
+        $scope.signinCredentials = {
+            email:'',
+            password: ''
+        };
+        $scope.registerCredentials = {
+            name: '',
+            email: '',
+            password: '',
+            avatar: 'default-user-avatar-0.png'
+        };
+
+        $scope.signIn = function () {
+            var proceed = true;
+            if($scope.signinCredentials.email === '' || angular.isUndefined($scope.signinCredentials.email) ){
+                proceed = false;
+            }
+            if($scope.signinCredentials.password === '' || angular.isUndefined($scope.signinCredentials.password) ){
+                proceed = false;
+            }
+            if(!proceed){
+                $scope.loginError = true;
+            }
+            else{
+                $scope.loginError = false;
+            }
+
+        };
+
+        $scope.register = function () {
+
+
+        };
+
+
 
     });
